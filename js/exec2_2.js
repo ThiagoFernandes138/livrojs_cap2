65cc00dfd1a97c7ddf6013d27b4e51654dc32e0f
ThiagoFernandes138 <150919764+ThiagoFernandes138@users.noreply.github.com>
2025-02-12 21:54:25 -0300

    Atualizando o código de acordo com a nova edição do livro.

--- js/exec2_2.js
+++ js/exec2_2.js
@@ -1,14 +1,15 @@
-function calculoValor(){
-    var inValor = document.getElementById("inValor");
-    var inTempoCliente = document.getElementById("inTempoCliente");
-    var outValor = document.getElementById("outValor");
-    var outTempoCliente = document.getElementById("outTempoCliente");
-    
-    var valor = Number(inValor.value);
-    var tempoCliente = Number(inTempoCliente.value);
-    var calculoTempoCliente = Math.ceil(tempoCliente / 15);
-    var total = calculoTempoCliente * valor;
-    outValor.textContent = "Valor a Pagar R$: " + total.toFixed(2);
-}
-var btCalcular = document.getElementById("btCalcular");
-btCalcular.addEventListener("click", calculoValor);
+    const frm = document.querySelector("form")
+    const resp1 = document.querySelector("#outValor")
+
+    frm.addEventListener("submit", (e)=>{
+        const valor = Number(frm.inValor.value)
+        const tempoCliente = Number(frm.inTempoCliente.value)
+        const calculoTempo = Math.ceil(tempoCliente / 15)
+        const total = calculoTempo * valor
+        resp1.innerText = `Valor a Pagar: R$ ${total.toFixed(2)}`
+        
+        e.preventDefault()
+
+    })
+  
+   
