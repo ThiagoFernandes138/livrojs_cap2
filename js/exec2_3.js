74b4ac3d3609c7a282c0b37f74fd0e91af44d615
ThiagoFernandes138 <150919764+ThiagoFernandes138@users.noreply.github.com>
2025-02-13 20:25:25 -0300

    Atualização de código de acordo com a nova edição do livro

--- js/exec2_3.js
+++ js/exec2_3.js
@@ -1,17 +1,16 @@
-function verPromocao(){
-var inProduto = document.getElementById("inProduto");
-var inPreco = document.getElementById("inPreco");
-var outProduto = document.getElementById("outProduto");
-var outPreco = document.getElementById("outPreco");
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outProduto");
+const resp2 = document.querySelector("#outPreco");
 
-var produto = inProduto.value;
-var preco = Number(inPreco.value);
+frm.addEventListener("submit", (e)=> {
+    const produto = frm.inProduto.value
+    const preco = Number(frm.inPreco.value)
+    const precoPromocao = preco * 2 + (preco / 2)
+    const terceiroProduto = preco / 2
 
-var precoPromocao = preco * 2 + (preco / 2);
-var terceiroProduto = preco / 2;
+    resp1.innerText = `${produto} - Promoção: Leve 3 por: R$ ${precoPromocao.toFixed(2)}`
+    resp2.innerText = `O 3° produto custa apenas: R$ ${terceiroProduto.toFixed(2)}`
+    e.preventDefault()
+})
 
-outProduto.textContent =  produto + " - Promoção: Leve 3 por R$: " + precoPromocao.toFixed(2);
-outPreco.textContent = "O 3 produto custa apenas R$: " + terceiroProduto.toFixed(2);
-}
-var btVerPromocao = document.getElementById("btVerPromocao");
-btVerPromocao.addEventListener("click", verPromocao);
+
